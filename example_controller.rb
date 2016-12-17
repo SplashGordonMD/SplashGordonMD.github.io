18527d8f035e6b5b09d67cc7d93dfdcdfc5a654a
Casey Platt <kse0824@gmail.com>
2016-12-17 14:58:50 -0600

    personel landing page

--- example_controller.rb
+++ example_controller.rb
@@ -1,5 +1,5 @@
 class ExampleController < ApplicationController
   def fortune_method
-  fortunes = ["You will be a billionare', "You will be hungry in an hour", "You will die in 7 days"]
+  fortunes = ["You will be a billionare", "You will be hungry in an hour", "You will die in 7 days"]
   @fortune = fortunes.sample
   
